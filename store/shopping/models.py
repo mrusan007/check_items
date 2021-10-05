815ead531e16f610a78bccda0e31191c0cc1e8f7
mrusan <mrusan@carnet.hr>
2021-10-05 21:43:50 +0200

    promotions, itemquantity and order in models.py

--- store/shopping/models.py
+++ store/shopping/models.py
@@ -1,9 +1,29 @@
 from django.db import models
 
 # Create your models here.
-class Items(models):
+class Items(models.Model):
     '''
     Items for our store.
     '''
     name = models.CharField(max_length=256, null=False)
     price = models.FloatField(null=False)
+
+class Promotions(models.Model):
+    '''
+    Promotions for our store.
+    '''
+    name = models.CharField(max_length=256, null=False)
+
+class ItemsQuantity(models.Model):
+    '''
+    How many of items per order.
+    '''
+    quantity = models.IntegerField(null=False)
+    items = models.ForeignKey(Items, on_delete=models.CASCADE)
+
+class Order(models.Model):
+    '''
+    Order from our customer.
+    '''
+    item_quantity = models.ManyToManyField(ItemsQuantity)
+    promotions = models.ManyToManyField(Promotions)
